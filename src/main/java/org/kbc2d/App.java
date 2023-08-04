69972bf6b29068c1add3fc6f1962f45821cf10ca
Minh Chau <lilichi4423@gmail.com>
2023-08-04 21:01:45 +0700

    Change layout

--- src/main/java/org/kbc2d/App.java
+++ src/main/java/org/kbc2d/App.java
@@ -21,6 +21,8 @@ public class App extends Application {
         StackPane stackPane = new StackPane(canvas);
         Scene scene = new Scene(stackPane);
         GameVars.put("scene", scene);
+
+
         Input.attachEventHandle(scene);
         stage.setScene(scene);
         stage.show();
--- src/main/java/org/kbc2d/scene/HomeScene.java
+++ src/main/java/org/kbc2d/scene/HomeScene.java
@@ -16,7 +16,17 @@ public class HomeScene extends BaseScene{
     ButtonGame exitButton;
     ButtonGame pveModeButton;
     ButtonGame settingsButton;
+    ButtonGame trainingModeButtonC;
+    ButtonGame pvpModeButtonC;
+    ButtonGame exitButtonC;
+    ButtonGame pveModeButtonC;
+    ButtonGame settingsButtonC;
 
+    ButtonGame train;
+    ButtonGame pve;
+    ButtonGame pvp;
+    ButtonGame settings;
+    ButtonGame exit;
     BackgroundGame background;
     VBox vBox;
 
@@ -24,13 +34,24 @@ public class HomeScene extends BaseScene{
     public HomeScene() {
         background = new BackgroundGame("asset/textures/ui/rectMenu/mainMenuBg.png");
 //        buttonExample = new ButtonExample(100, 100);
-        trainingModeButton = new ButtonGame("asset/textures/ui/rectMenu/continue.png", 300, 100);
-        pvpModeButton = new ButtonGame("asset/textures/ui/rectMenu/newGame.png", 300, 200);
-        settingsButton = new ButtonGame("asset/textures/ui/rectMenu/settings.png", 300, 400);
-        exitButton = new ButtonGame("asset/textures/ui/rectMenu/quit.png", 300, 500);
-        pveModeButton = new ButtonGame("asset/textures/ui/rectMenu/quit.png", 300, 300);
+        trainingModeButton = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/trainingMode.png", 100, 250);
+        pvpModeButton = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/fightingMode.png", 100, 320);
+        settingsButton = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/Settings.png", 100, 390);
+        exitButton = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/Exit.png", 600, 250);
+        pveModeButton = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/selfMode.png", 600, 320);
+
+        trainingModeButtonC = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/trainingModeClick.png", 100, 250);
+        pvpModeButtonC = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/fightingModeClick.png", 100, 320);
+        settingsButtonC = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/SettingsClick.png", 100, 390);
+        exitButtonC = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/ExitClick.png", 600, 250);
+        pveModeButtonC = new ButtonGame("asset/textures/ui/rectMenu/ButtonSet/selfModeClick.png", 600, 320);
 
 
+        train = trainingModeButton;
+        pvp = pvpModeButton;
+        pve = pveModeButton;
+        settings = settingsButton;
+        exit = exitButton;
     }
 
     @Override
@@ -39,15 +60,17 @@ public class HomeScene extends BaseScene{
 //        buttonExample.render();
 
 
-        trainingModeButton.render();
-        pvpModeButton.render();
-        exitButton.render();
-        pveModeButton.render();
-        settingsButton.render();
+        train.render();
+        pvp.render();
+        exit.render();
+        pve.render();
+        settings.render();
+
     }
 
     @Override
     public void update(float deltaTime) {
+
 
     }
 
@@ -58,25 +81,62 @@ public class HomeScene extends BaseScene{
 //                    buttonExample.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY());
 //                }
 //        );
+
+        GameVars.get("scene", Scene.class).setOnMouseMoved(
+                mouseEvent -> {
+
+                    if (trainingModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+                        train = trainingModeButtonC;
+                    }
+                    else {
+                        train =trainingModeButton;
+                    }
+                    if (pvpModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+                        pvp = pvpModeButtonC;
+                    }
+                    else {
+                        pvp =pvpModeButton;
+                    }
+                    if (exitButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+                        exit = exitButtonC;
+                    }
+                    else {
+                        exit =exitButton;
+                    }
+                    if (pveModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+                        pve = pveModeButtonC;
+                    }
+                    else {
+                        pve =pveModeButton;
+                    }
+                    if (settingsButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+                        settings = settingsButtonC;
+                    }
+                    else {
+                        settings =settingsButton;
+                    }
+                });
+
         GameVars.get("scene", Scene.class).setOnMouseClicked(
                 mouseEvent -> {
+
                     if (trainingModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
                         SceneManager.setCurrentScene(SceneType.TRAINING_SCENE);
                     }
-                    else if (pvpModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
-                        SceneManager.setCurrentScene(SceneType.PvP_SCENE);
-                    }
-                    else if (pveModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
-                        SceneManager.setCurrentScene(SceneType.PvE_SCENE);
-                    }
-                    else if (settingsButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
-                        SceneManager.setCurrentScene(SceneType.SETTING_SCENE);
-                    }
-                    else if (exitButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
-                        System.exit(0);
-                    }
-
-
+//                    else if (pvpModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+//                        SceneManager.setCurrentScene(SceneType.PvP_SCENE);
+//                    }
+//                    else if (pveModeButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+//                        SceneManager.setCurrentScene(SceneType.PvE_SCENE);
+//                    }
+//                    else if (settingsButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+//                        SceneManager.setCurrentScene(SceneType.SETTING_SCENE);
+//                    }
+//                    else if (exitButton.handleClick((float) mouseEvent.getX(), (float) mouseEvent.getY())) {
+//                        System.exit(0);
+//                    }
+//
+//
                 }
         );
 
